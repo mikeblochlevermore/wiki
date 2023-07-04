3569f76d453033b5204f9721f63a03ab8aa73459
Mike Bloch-Levermore <interactivephilosophy@gmail.com>
2023-07-04 17:56:08 +0200

    added basic random function

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,11 +1,21 @@
 from django.urls import path
+import random
+from . import util
 
 from . import views
+
+# calculation to choose a random page
+
+random_page = random.choice(util.list_entries())
+# random_page = "git"
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("create", views.create, name="create"),
 
     # path that serves all entries
-    path("wiki/<str:title>", views.entry, name="entry")
+    path("wiki/<str:title>", views.entry, name="entry"),
+
+    # path to a random page
+    path(f"wiki/{random_page}", views.entry, name="random")
 ]
