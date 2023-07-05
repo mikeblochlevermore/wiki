b2184213e3219073e3a030923ed4e6bfd20b2d8b
Mike Bloch-Levermore <interactivephilosophy@gmail.com>
2023-07-05 17:04:19 +0200

    search displays partial matches

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -36,8 +36,31 @@ def random_page(request):
     title = random.choice(util.list_entries())
     return entry(request, title)
 
+# Function for searching the wiki
 def search(request):
+    # get the entry from the search field
     query = request.GET.get('q')
-    return render(request, "encyclopedia/search.html", {
-        "query": query,
-    })
+
+    # check whether the entry is an exact match to a title
+    content = util.get_entry(query)
+
+    if content is None:
+
+        partial_match = partial_matches(query, util.list_entries())
+
+        return render(request, "encyclopedia/search.html", {
+            "query": query,
+            "partial_match": partial_match,
+        })
+    else:
+        title = query
+        return entry(request, title)
+
+# function to look for partial matches between the query and exisitng titles
+def partial_matches(query, entries):
+    partial_match = []
+    for entry in entries:
+        # ignores capitalisation in search results
+        if query.lower() in entry.lower():
+            partial_match.append(entry)
+    return partial_match
