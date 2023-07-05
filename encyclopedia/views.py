9531fcc96da2e448ab57cfdbc391465cc67008fe
Mike Bloch-Levermore <interactivephilosophy@gmail.com>
2023-07-05 12:09:20 +0200

    error functionality implemented

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 import markdown2
 import random
+from urllib.error import HTTPError
 
 from . import util
 
@@ -15,15 +16,23 @@ def create(request):
     })
 
 def entry(request, title):
-    return render(request, "encyclopedia/entry.html", {
-        "title": title,
-        "content": markdown2.markdown(util.get_entry(title))
-    })
+    try:
+        content = util.get_entry(title)
+        if content is None:
+            raise HTTPError(None, 404, "Entry not found", None, None)
+        # Got help from Chat GPT for the above line
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": markdown2.markdown(content)
+        })
+    except HTTPError:
+        return error(request)
 
+def error(request):
+    return render(request, "encyclopedia/error.html")
+
+# The random_page function calls the entry function with a random title from the list of entries
 def random_page(request):
     title = random.choice(util.list_entries())
     return entry(request, title)
 
-def error(request):
-    return render(request, "encyclopedia/error.html", {
-    })
